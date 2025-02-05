2d9bbdc13ce12446e4631a49aeca058609f95938
Arun-2010 <arunbandari15@gmail.com>
2025-02-05 11:58:53 +0530

    Initial commit

---
+++ dom projects/add friend feature with the same button/script.js
@@ -0,0 +1,21 @@
+var istatus=document.querySelector("h5")
+
+var addFriend=document.querySelector("#add")
+
+var remove=document.querySelector("#remove")
+
+
+addFriend.addEventListener("click",function(){
+   istatus.innerHTML="Friends"
+   istatus.style.color="green "
+
+
+})
+
+
+remove.addEventListener("click",function(){
+    istatus.innerHTML="Stranger"
+    istatus.style.color="red "
+ 
+ 
+ })
